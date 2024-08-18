9eff2a38767135e2a2b204c8b8b5d3f45d12a900
anit172737 <anitdhadve17@gmail.com>
2024-08-18 18:23:11 +0530

    serveo tunnel service implemented

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,6 @@
-const connect = require("./dbConfig/dbConfig");
 const express = require("express");
 const cors = require("cors");
+const connect = require("./dbConfig/dbConfig");
 const userRoutes = require("./routes/users/userRoutes");
 const javascriptRoutes = require("./routes/interview/javascript/javascriptRoutes");
 const reactRoutes = require("./routes/interview/reactjs/reactjsRoutes");
@@ -8,46 +8,45 @@ const nextRoutes = require("./routes/interview/nextjs/nextjsRoutes");
 const typescriptRoutes = require("./routes/interview/typescript/typescriptRoutes");
 const upload = require("./middleware/uploader");
 const app = express();
-// const bcryptjs = require("bcryptjs");
 const port = 8000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// Define allowed origins
+// Enable CORS for all routes
 const allowedOrigins = [
-  "http://localhost:8000", // Local development
-  "https://reacto2.vercel.app/", // Your Vercel app URL
-  "https://abcd1234.loca.lt", // Replace with your Localtunnel URL
+  "http://localhost:3000", // Local development
+  "https://reacto2.vercel.app", // Your Vercel app URL
+  "https://2b58cd70309ae02d4b66820f51a2f3a5.serveo.net", // Your Localtunnel URL
 ];
 
-// Configure CORS middleware
+// CORS configuration
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-
-      // Check if the origin is in the allowedOrigins array
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
+// app.options("*", cors());
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use("/uploads", express.static("../uploads"));
 
+// Connect to the database
 connect();
 
+// Define routes
 app.use("/api/user", userRoutes);
 app.use("/api/javascript", javascriptRoutes);
 app.use("/api/react", reactRoutes);
 app.use("/api/next", nextRoutes);
 app.use("/api/type", typescriptRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
